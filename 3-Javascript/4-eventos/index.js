3ef901779b5080b3aa1c25d125787b1cbf267d9c
ludovale <lu.v.cavalcante@gmail.com>
2023-11-09 21:54:43 -0300

    eventos2

---
+++ 3-Javascript/4-eventos/index.js
@@ -0,0 +1,66 @@
+/**
+ * EVENTOS:
+ * onclick
+ * onchage
+ * onblur
+ * onkeydown
+ * onkeyup
+ * onsubmit
+ */
+
+// ------------------ONCLICK
+
+// function eventoBotao(){
+// //let dados = document.getElementById("mudanca").innerHTML = "vocÊ clicou no botão";
+
+// // mudanca.innerHTML = "vocÊ clicou no botão";
+
+// let div = document.createElement("div");
+// div.classList.add("centered-div");
+// document.getElementById("container").appendChild(div);
+
+// }
+
+
+
+// ------------------ONCHANGE
+
+
+// function minhaFuncao() {
+//     let campo = document.getElementById("meuCampo");
+
+//     alert(campo.value);
+// }
+
+
+
+// ------------------ONBLUR
+//bootstrap adicionado para essa parte
+
+// function validarCampo(){
+
+//     let validarCampoBlu = document.getElementById("meucampoValidado");
+
+//     if (validarCampoBlu.value.length === 0) {
+//         validarCampoBlu.classList.remove("is-valid", "form-control");
+//         validarCampoBlu.classList.add("is-invalid", "form-control");
+
+//     } else {
+//         validarCampoBlu.classList.remove("is-invalid");
+//         validarCampoBlu.classList.add("is-valid", "form-control");
+//     }
+
+// }
+
+// ------------------ONKEYDOWN
+//bootstrap adicionado para essa parte
+
+function verificarTecla(event){
+    let alteracao = document.getElementById("meuBotaoEstilizado");
+
+    if(event.key === 'Enter'){
+        alert("Você apertou enter e modificou algo na tela"); //RECONHECEU O ENTER E DEU UM ALERT
+        alteracao.classList.add("btn", "btn-sucess"); //ADIÇÃO DE CLASSES DO BOOTSTRAP NO BOTAO
+    }
+}
+
