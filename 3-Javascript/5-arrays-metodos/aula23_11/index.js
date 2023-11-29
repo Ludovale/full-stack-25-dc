afde0a77cd77f02908f7ccdf45d0619f306ff3ae
ludovale <lu.v.cavalcante@gmail.com>
2023-11-28 21:53:01 -0300

    aula 28 do 11 - lista objetos array

--- 3-Javascript/5-arrays-metodos/aula23_11/index.js
+++ 3-Javascript/5-arrays-metodos/aula23_11/index.js
@@ -3,52 +3,72 @@
 //https://dontpad.com/fs-25-metodos
 //Tarefa em aula: for of ou in - acessar indexx - passar valor dentro - nome[i] para acessar o valor - colocar dentro do alert no li.
 
-const convidados = [];
+const nomes = [];
 let actualId = "";
 const botao = document.getElementById("addItem");
 const inputName = document.getElementById("userName");
+const inputEmail = document.getElementById("userEmail");
+const inputPhone = document.getElementById("userPhone");
 const lista = document.getElementById("lista");
-const inputEdit = document.getElementById("editName");
+const inputEditName = document.getElementById("editName");
+const inputEditEmail = document.getElementById("editEmail");
+const inputEditPhone = document.getElementById("editPhone");
 const btnEdit = document.getElementById("btnEdit");
 
 
 function handleDelete(id) {
-    alert(id);
+    nomes.splice(id, 1);
+    handleList();
 }
 
 function handleEdit(id) {
-    inputEdit.value = convidados[id];
-    actualId = id;
+    inputEditName.value = nomes[id].name;
+    inputEditEmail.value = nomes[id].email;
+    inputEditPhone.value = nomes[id].phone;
+    actualId = id; //salvou o id de forma global dentro da variável
 }
 
 function confirmEdit() {
-convidados[actualId] = inputEdit.value;
+if (actualId !== ""){ //sempre o numero de convidados for maior que 0 (ou seja, tenha mais de 1) e que o valor do id não for nulo (definido na propria funçao) ;
+nomes[actualId].name = inputEditName.value;
+nomes[actualId].email = inputEditEmail.value;
+nomes[actualId].phone = inputEditPhone.value;
 handleList();
-}
+actualId = "";
+inputEditName.value = "";
+inputEditEmail.value = "";
+inputEditPhone.value = "";
+}}
 
 function handleList() {
     lista.innerHTML = "";
-    for (const index in convidados) {
+    for (const index in nomes) {
         lista.innerHTML += `<li 
         class="list-item">
-         ${convidados[index]}
+         ${nomes[index].name}, ${nomes[index].email}, ${nomes[index].phone}
         <div class="btns-action">
         <button class="button-edit" onclick="handleEdit(${index})">Editar</button>
         <button class="button-delete" onclick="handleDelete(${index})">Deletar</button>
-        </div> </li>`;
+        </div> </li>`; //UM item da lista é adicionado, mais os DOIS botoes de EDITAR e DELETAR;
     }
-    console.log(convidados);
+    console.log(nomes);
     }
 
 function handleCreateUser() {
-    convidados.push(inputName.value);
-    console.log(convidados);
+    nomes.push({
+        name: inputName.value,
+        email: inputEmail.value,
+        phone: inputPhone.value,
+    });
+    console.log(nomes);
     inputName.value = "";
+    inputEmail.value = "";
+    inputPhone.value = "";
 }
 
 botao.addEventListener("click", () => {
-    handleCreateUser()
-    handleList()
+    handleCreateUser();
+    handleList();
 
 btnEdit.addEventListener("click", confirmEdit);
 
