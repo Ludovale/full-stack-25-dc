ef0e3f64b61395b3ccd47654920c0add253afb33
ludovale <lu.v.cavalcante@gmail.com>
2023-12-19 21:49:44 -0300

    async 2

--- 3-Javascript/6-assincrona/index.js
+++ 3-Javascript/6-assincrona/index.js
@@ -5,7 +5,7 @@
 //403 - autorização(clearence) de usuario
 //404 - page not found
 
-fetch('https://fakestoreapi.com/products')
+fetch('https://6580c8963dfdd1b11c421705.mockapi.io/clientes')
             .then(res=>res.json())
             .then(json=>console.log(json))
 
@@ -14,10 +14,54 @@ fetch('https://fakestoreapi.com/products')
 //no caso seria "e então, me retorne a resposta (res.json)"
 //depois  (então - .the)  põe esse .json no console.log
 
-async function fetchProducts() {
-    const response = await fetch('https://fakestoreapi.com/products');
+const tableContent = document.getElementById("tableContent");
+
+async function fetchClients() {
+    const response = await fetch('https://6580c8963dfdd1b11c421705.mockapi.io/clientes');
+
     const data = await response.json();
-    console.log(data);
+
+    return data;
+//    console.log(data);
 }
 
-fetchProducts();
+async function deleteClient(id){
+    const response = await fetch(
+        `https://6580c8963dfdd1b11c421705.mockapi.io/clientes/${id}` , {
+            method: "DELETE", 
+        }
+    );
+    await handleClientsList();
+}
+
+function mountList(arr) {
+    tableContent.innerHTML = "";
+    arr.map((valor) => { //// o valor quer dizer cada objeto
+
+    return (tableContent.innerHTML += `
+        <tr>
+        <td>${valor.name} - ${valor.lastName}</td>  <td>${valor.email}</td> 
+
+        <td>
+        <div>
+        <button>
+          Editar
+        </button>
+        <button onclick = "deleteClient(${valor.id})">
+          Deletar
+        </button>
+      </div>
+      </td>
+        </tr>
+        `)
+        
+    });
+}
+
+async function handleClientsList(){
+    const clients = await fetchClients();
+    if (clients.length > 0){
+        mountList(clients); //chama a função citada anterior do mountList
+    }
+}
+handleClientsList();
