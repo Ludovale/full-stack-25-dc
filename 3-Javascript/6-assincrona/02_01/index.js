bf9dde641bb281df5c3ba4cfbd0bd7806efe1548
Ludovale <lu.v.cavalcante@gmail.com>
2024-01-04 21:54:44 -0300

    css feito e inicio do crud do js com delete

--- 3-Javascript/6-assincrona/02_01/index.js
+++ 3-Javascript/6-assincrona/02_01/index.js
@@ -1,11 +1,39 @@
+const tableContent = document.getElementById("tableContent");
+const url = "https://65949ae91493b011606aad62.mockapi.io/clientes";
+
 async function fetchClients() {
-    //esse RESPONSE é um pré-processo para acessar as informações
-    const response = await fetch(
-        "https://65949ae91493b011606aad62.mockapi.io/clientes"
-    ); 
-    //esse DATA é o que recolhe as informações do .json
-    const data = await response.json();
-    console.log(data);
+  //esse RESPONSE é um pré-processo para acessar as informações
+  const response = await fetch(url);
+  //esse DATA é o que recolhe as informações do .json
+  const data = await response.json();
+  return data;
 }
 
-fetchClients();
+async function deleteClients(id) {
+    const response = await fetch(`${url}/${id}`, { method: "DELETE" });
+    const data = await response.json();
+    await renderClientList();
+}
+
+async function renderClientList() {
+    tableContent.innerHTML = "";
+    const clients = await fetchClients();
+    clients.map((client) => {
+    return (tableContent.innerHTML += `
+    <tr>
+    <td class="body-cell">${client.name}</td>
+    <td class="body-cell">${client.email}</td>
+    <td class="body-cell">${client.telefone}</td>
+    <td class="body-cell actions">
+        <div class="actions-buttons">
+        <button class="edit">Editar</button>
+        <button class="delete" onclick="deleteClients(${client.id})">Excluir</button></td>
+        </div>
+</tr>
+`);
+  });
+}
+
+window.addEventListener("load", async () => {
+await  renderClientList();
+});
